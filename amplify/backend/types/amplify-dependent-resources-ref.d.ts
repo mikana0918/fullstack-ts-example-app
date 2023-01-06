50bc8665bfbe156066325aa62a91b41cb66175f5
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-06 18:21:24 +0900

    feat: Add amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "frouriosample": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "uploads": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
