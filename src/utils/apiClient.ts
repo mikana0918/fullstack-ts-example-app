aeba6405776af95eb223310aef7cf3ab11e0de34
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-09 01:45:40 +0900

    feat: Add axios interceptor

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,4 +1,22 @@
+import axios from 'axios'
 import aspida from '@aspida/axios'
 import api from '$/api/$api'
+import { Amplify } from 'aws-amplify'
 
-export const apiClient = api(aspida())
+const instance = axios.create()
+
+instance.interceptors.request.use(async (config) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const token = await Amplify.Auth.currentSession().then((res: any) => {
+    const accessToken = res.getAccessToken()
+    const jwt = accessToken.getJwtToken()
+
+    return jwt
+  })
+
+  config.headers = { Authorization: `Bearer ${token}` }
+
+  return config
+})
+
+export const apiClient = api(aspida(instance))
