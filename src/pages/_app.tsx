1549d1735c84b7072210fd4803277f121132a351
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-09 19:03:45 +0900

    feat: Add useAuth hooks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,19 +6,20 @@ import { Amplify } from '@aws-amplify/core'
 import '@aws-amplify/ui-react/styles.css'
 import awsExports from '~/aws-exports'
 import { Authenticator } from '@aws-amplify/ui-react'
+import { AuthProvider } from '~/providers/auth/AuthProvider'
 
 Amplify.configure(awsExports)
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Authenticator>
-      {({ user: authUser }) => (
-        <div>
+      {({ user }) => (
+        <AuthProvider user={user}>
           <Head>
             <link rel="icon" href={staticPath.favicon_png} />
           </Head>
-          <Component authUser={authUser} {...pageProps} />
-        </div>
+          <Component {...pageProps} />
+        </AuthProvider>
       )}
     </Authenticator>
   )
