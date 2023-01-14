fe74c27a894c0c1c91e092365e34633c0dfba457
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-15 04:41:27 +0900

    chore: Remove unused parts

--- server/domains/services/user.ts
+++ server/domains/services/user.ts
@@ -1,7 +1,4 @@
-import fs from 'fs'
-import path from 'path'
 import type { MultipartFile } from '@fastify/multipart'
-import { API_ORIGIN, API_USER_ID, API_USER_PASS, API_UPLOAD_DIR } from '$/env'
 import { s3Service } from '$/app/services/storage/handler'
 import { storagePaths } from '$/app/services/storage/paths'
 import { CognitoUserId } from '$/domains/ValueObjects/Auth/CognitoUserId'
@@ -9,49 +6,6 @@ import { PrismaClient } from '@prisma/client'
 import type { User } from '@prisma/client'
 
 const prisma = new PrismaClient()
-
-const iconsDir = API_UPLOAD_DIR && path.resolve(API_UPLOAD_DIR, 'icons')
-
-const createIconURL = (dir: string, name: string) =>
-  `${API_ORIGIN}/${dir}icons/${name}`
-
-const getUserIconName = () => {
-  return 'user-icon'
-}
-
-const getUserInfo = () => {
-  const iconName = getUserIconName()
-  return {
-    name: 'sample user',
-    icon:
-      iconsDir && fs.existsSync(path.resolve(iconsDir, iconName))
-        ? createIconURL('upload/', iconName)
-        : createIconURL('static/', 'dummy.svg')
-  }
-}
-
-export const validateUser = (id: string, pass: string) =>
-  id === API_USER_ID && pass === API_USER_PASS
-
-export const changeIcon = async (id: string, iconFile: MultipartFile) => {
-  const iconName = getUserIconName()
-
-  if (!iconsDir) {
-    throw new Error('API_UPLOAD_DIR is not configured.')
-  }
-
-  await fs.promises.mkdir(iconsDir, { recursive: true })
-
-  await fs.promises.writeFile(
-    path.resolve(iconsDir, iconName),
-    await iconFile.toBuffer()
-  )
-
-  return {
-    id,
-    ...getUserInfo()
-  }
-}
 
 export const uploadUserIcon = async ({
   iconFile,
