fc0f4f89fa1db15ad8b161d3e9aedae085c0f31a
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-16 01:51:57 +0900

    refactor: Make env.ts more reporable

--- server/env.ts
+++ server/env.ts
@@ -2,18 +2,24 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const NODE_ENV = process.env.NODE_ENV
-const API_JWT_SECRET = process.env.API_JWT_SECRET ?? ''
-const API_USER_ID = process.env.API_USER_ID ?? ''
-const API_USER_PASS = process.env.API_USER_PASS ?? ''
-const API_SERVER_PORT = +(process.env.API_SERVER_PORT ?? '8080')
-const API_BASE_PATH = process.env.API_BASE_PATH ?? ''
-const API_ORIGIN = process.env.API_ORIGIN ?? ''
-const API_UPLOAD_DIR = process.env.API_UPLOAD_DIR ?? ''
+const read = (envVar?: string) => {
+  const defaultEmptyValue = '!!!THIS_VALUE_IS_NOT_SET!!!'
 
-const AWS_REGION = process.env.AWS_REGION ?? ''
-const AWS_S3_ENDPOINT = process.env.AWS_S3_ENDPOINT ?? ''
-const AWS_S3_BUCKET_USER_UPLOADS = process.env.AWS_S3_BUCKET_USER_UPLOADS ?? ''
+  return envVar ?? defaultEmptyValue
+}
+
+const NODE_ENV = process.env.NODE_ENV
+const API_JWT_SECRET = read(process.env.API_JWT_SECRET)
+const API_USER_ID = read(process.env.API_USER_ID)
+const API_USER_PASS = read(process.env.API_USER_PASS)
+const API_SERVER_PORT = +(process.env.API_SERVER_PORT ?? '8080')
+const API_BASE_PATH = read(process.env.API_BASE_PATH)
+const API_ORIGIN = read(process.env.API_ORIGIN)
+const API_UPLOAD_DIR = read(process.env.API_UPLOAD_DIR)
+
+const AWS_REGION = read(process.env.AWS_REGION)
+const AWS_S3_ENDPOINT = read(process.env.AWS_S3_ENDPOINT)
+const AWS_S3_BUCKET_USER_UPLOADS = read(process.env.AWS_S3_BUCKET_USER_UPLOADS)
 
 export {
   API_JWT_SECRET,
