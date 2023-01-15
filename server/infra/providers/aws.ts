24d438d034282fbeff8c0c3193e63fbf6646626e
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-16 02:16:53 +0900

    feat: Add creds for AWS

--- server/infra/providers/aws.ts
+++ server/infra/providers/aws.ts
@@ -3,6 +3,9 @@ import { AWS_REGION } from '$/env'
 
 export const registerProviderAWS = () => {
   AWS.config.update({
+    credentials: new AWS.EnvironmentCredentials(
+      'AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY'
+    ),
     region: AWS_REGION,
     s3ForcePathStyle: true
   })
