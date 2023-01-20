fe69022d735c40708aadcea9293fee1564150971
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-20 18:54:17 +0900

    feat: Inject chakra-ui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,21 +7,35 @@ import '@aws-amplify/ui-react/styles.css'
 import awsExports from '~/aws-exports'
 import { Authenticator } from '@aws-amplify/ui-react'
 import { AuthProvider } from '~/providers/auth/AuthProvider'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 
 Amplify.configure(awsExports)
 
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac'
+  }
+}
+
+const theme = extendTheme({ colors })
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Authenticator>
-      {({ user }) => (
-        <AuthProvider user={user}>
-          <Head>
-            <link rel="icon" href={staticPath.favicon_png} />
-          </Head>
-          <Component {...pageProps} />
-        </AuthProvider>
-      )}
-    </Authenticator>
+    <ChakraProvider theme={theme}>
+      <Authenticator>
+        {({ user }) => (
+          <AuthProvider user={user}>
+            <Head>
+              <link rel="icon" href={staticPath.favicon_png} />
+            </Head>
+            <Component {...pageProps} />
+          </AuthProvider>
+        )}
+      </Authenticator>
+    </ChakraProvider>
   )
 }
 
