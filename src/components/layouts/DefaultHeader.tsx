29d6ad20cded495419570c3e2f42557c0e36dac1
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-21 18:55:25 +0900

    refactor: Header to move edit Icon feature outside

--- src/components/layouts/DefaultHeader.tsx
+++ src/components/layouts/DefaultHeader.tsx
@@ -1,39 +1,25 @@
-import { ChangeEvent } from 'react'
 import styles from './DefaultHeader.module.scss'
 import { AmplifyAuthModule } from '~/app/services/auth'
 import { useAuth } from '~/hooks/useAuth'
-import { apiClient } from '~/utils/apiClient'
-import { Box, Button, HStack, Input } from '@chakra-ui/react'
+import { Box, Button, HStack, Flex } from '@chakra-ui/react'
 
 const DefaultHeader = () => {
-  const { cognitoUser, mutate: mutateAuthUser } = useAuth()
-
-  const editIcon = async (evt: ChangeEvent<HTMLInputElement>) => {
-    if (evt.target.files) {
-      await apiClient.users.post({ body: { file: evt.target.files[0] } })
-      mutateAuthUser()
-    }
-  }
+  const { cognitoUser } = useAuth()
 
   const logout = async () => {
     await AmplifyAuthModule.signOut()
   }
 
   return (
-    <Box>
-      <Box className={styles.userBanner}>
-        <HStack>
-          {cognitoUser ? (
-            <Box>
-              <HStack>
-                <Input type="file" accept="image/*" onChange={editIcon} />
-                <Button onClick={logout}>Logout</Button>
-              </HStack>
-            </Box>
-          ) : null}
-        </HStack>
-      </Box>
-    </Box>
+    <Flex className={styles.userBanner} justifyContent={'flex-end'}>
+      {cognitoUser ? (
+        <Box>
+          <HStack>
+            <Button onClick={logout}>Logout</Button>
+          </HStack>
+        </Box>
+      ) : null}
+    </Flex>
   )
 }
 
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,8 +1,28 @@
+import { ChangeEvent } from 'react'
+import { useAuth } from '~/hooks/useAuth'
+import { apiClient } from '~/utils/apiClient'
+import { Box, HStack, Input } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import Layout from '~/layouts/DefaultLayout'
 
 const Settings: NextPage = () => {
-  return <>This is settings page!</>
+  const { mutate: mutateAuthUser } = useAuth()
+
+  const editIcon = async (evt: ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.files) {
+      await apiClient.users.post({ body: { file: evt.target.files[0] } })
+      mutateAuthUser()
+    }
+  }
+
+  return (
+    <>
+      <Box>
+        <HStack>
+          <Input type="file" accept="image/*" onChange={editIcon} />
+        </HStack>
+      </Box>
+    </>
+  )
 }
 
 export default Settings
