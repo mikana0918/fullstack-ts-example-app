c6493dee6b93bcca832d2c9f2eed0dcc8f3cf64c
mikana0918 <shogo.kusuhara@gmail.com>
2023-01-22 17:02:51 +0900

    refactor: Setting page to add validation / toast feedback

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -6,28 +6,44 @@ import {
   FormControl,
   FormLabel,
   FormHelperText,
+  FormErrorMessage,
   Heading,
-  Image
+  Image,
+  useToast
 } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { useNextHeadMutation } from '~/store/useNextHeadMutation'
 import { Field, Form, Formik, FieldProps } from 'formik'
 import useAspidaSWR from '@aspida/swr'
 import { apiClient } from '~/utils/apiClient'
+import { z } from 'zod'
+import { toFormikValidationSchema } from 'zod-formik-adapter'
 
 interface FormValue {
   file?: File
   username: string
 }
 
+const formSchema = z.object({
+  file: z.custom<File>(),
+  username: z.string().max(191)
+})
+
 const ELEMENT_ID_INPUT_FILE_UPLOADER = 'file-upload'
 
 const Settings: NextPage = () => {
   useNextHeadMutation('設定')
 
+  const toast = useToast()
+
   const { data: me, mutate } = useAspidaSWR(apiClient.users.me)
 
-  const [inputImgPreviewURL, setInputImgPreviewURL] = useState<string>('')
+  const defaultPreviewURL = me?.icon_path
+    ? `${process.env.NEXT_PUBLIC_ASSET_ORIGIN_URL}/${me.icon_path}`
+    : ''
+
+  const [inputImgPreviewURL, setInputImgPreviewURL] =
+    useState<string>(defaultPreviewURL)
 
   const editIcon = async (
     evt: ChangeEvent<HTMLInputElement>
@@ -66,6 +82,14 @@ const Settings: NextPage = () => {
     })
 
     await mutate()
+
+    toast({
+      title: 'Account Updated 🎉',
+      description: "Hooray! We've updated your account for you.",
+      status: 'success',
+      duration: 4000,
+      isClosable: true
+    })
   }
 
   const initialValues: FormValue = {
@@ -77,30 +101,44 @@ const Settings: NextPage = () => {
     <>
       <Heading>Settings</Heading>
       <Box>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={toFormikValidationSchema(formSchema)}
+        >
           {({ values, setFieldValue }) => (
             <Form>
               <Field name="username">
-                {({ field, form }: FieldProps<FormValue, FormValue>) => (
-                  <FormControl as="fieldset" mb={4}>
-                    <FormLabel requiredIndicator={<span>*</span>}>
-                      Your Name
-                    </FormLabel>
-                    <Input
-                      {...field}
-                      placeholder={me?.user_name ?? 'John Doe'}
-                      type="text"
-                      name="username"
-                      value={values.username}
-                      required
-                    />
-                    <FormHelperText>Your username.</FormHelperText>
-                  </FormControl>
-                )}
+                {({ field, form }: FieldProps<FormValue, FormValue>) => {
+                  return (
+                    <FormControl
+                      as="fieldset"
+                      mb={4}
+                      isInvalid={!!form.errors.username}
+                    >
+                      <FormLabel>Your Name</FormLabel>
+                      <Input
+                        {...field}
+                        placeholder={me?.user_name ?? 'John Doe'}
+                        type="text"
+                        name="username"
+                        value={values.username}
+                        required
+                      />
+                      <FormErrorMessage>
+                        {form.errors.username}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )
+                }}
               </Field>
               <Field name="file">
-                {({ field, form }: FieldProps<FormValue, FormValue>) => (
-                  <FormControl as="fieldset" mb={4}>
+                {({ form }: FieldProps<FormValue, FormValue>) => (
+                  <FormControl
+                    as="fieldset"
+                    mb={4}
+                    isInvalid={!!form.errors.file}
+                  >
                     <FormLabel>Your profile picture</FormLabel>
                     <Image
                       cursor={'pointer'}
@@ -127,7 +165,7 @@ const Settings: NextPage = () => {
                       Recommended square-ish picture. Please make sure upload in
                       valid media types.
                     </FormHelperText>
-                    {/* <FormErrorMessage>{form.errors.query}</FormErrorMessage> */}
+                    <FormErrorMessage>{form.errors.file}</FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
